77db4869a5b11115294d424a215e8001d35fd5a8
Evan <104227735+Quintsi@users.noreply.github.com>
2023-12-13 23:22:05 -0500

    Update Appointment.java with comments and fixed issue with setAppointment method.

--- GroupFourTeamProject/src/Appointment.java
+++ GroupFourTeamProject/src/Appointment.java
@@ -1,25 +1,31 @@
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-public class Appointment {
+public class Appointment{
     
+    // LocalDateTime of current date
     LocalDateTime currentDateTime = LocalDateTime.now();
     int currentYear = currentDateTime.getYear();
     int currentMonth = currentDateTime.getMonthValue();
     int currentDay = currentDateTime.getDayOfMonth();
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss");
     
+    //variable of the class
     private LocalDateTime appointmentTime;
     private Object doctor;
     private Object patient;
 
 
+
+        //constructor
         Appointment(int month, int day, int time, Object doctor, Object patient){
             
            setAppointment(month, day, time, doctor, patient);
 
         }
 
+
+        //methods below
     @Override
     public String toString(){
         return "This appointment is set for " + appointmentTime.format(formatter) + " with " + this.doctor + " for " + this.patient;
@@ -27,7 +33,7 @@ public class Appointment {
 
     public void setAppointment(int month, int day, int time, Object doctor, Object patient){
 
-        if(day >= currentDay){
+        if(day >= currentDay + 5){
 
             this.appointmentTime = LocalDateTime.of(currentYear, month, day, time, 0, 0);
             this.doctor = doctor;
@@ -36,8 +42,6 @@ public class Appointment {
         } else {
             System.out.println("We are currently not taking appointments within this week, we ask that you set an appointment at least 5 days from now.");
         }
-
-
 
     }
 
